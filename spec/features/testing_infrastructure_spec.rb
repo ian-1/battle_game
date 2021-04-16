ecc6120aed8630f7b70dccaf87518f1e09b2f3fe
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-16 14:45:24 +0100

    Attacking Player 2

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -15,6 +15,14 @@ end
 feature 'See Player 2 hitpoints' do
   scenario 'player 1 can see player 2 hitpoints' do
     sign_in_and_play
-    expect(page).to have_content '10HP'
+    expect(page).to have_content '100HP'
+  end
+end
+
+feature 'Attack Player 2 and get confirmation' do
+  scenario 'player 1 can attack player 2 and see hit' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Player_2 has been hit'
   end
 end
